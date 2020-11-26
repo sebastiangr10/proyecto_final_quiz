0ba32fd017fe674598ddfbca29ee6156d42ff405
sebastiangr10 <sebastiangr1091@gmail.com>
2020-11-26 16:53:06 -0500

    proyecto

---
+++ quiz.js
@@ -0,0 +1,10 @@
+document.getElementById("categorias").style.display="none"
+
+function empezar(){
+    document.getElementById("instrucciones").style.display="none"
+    document.getElementById("categorias").style.display="block"
+}
+function regresar(){
+    document.getElementById("categorias").style.display="none"
+    document.getElementById("instrucciones").style.display="block"
+}
